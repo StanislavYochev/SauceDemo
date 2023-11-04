36828cb019b054ab6381e16f4d92a6e5dc376251
DESKTOP-DLHAJ9J\PC <stanislavyochev1994r@gmail.com>
2023-11-04 20:08:08 +0200

    sss

--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.w3c.dom.html.HTMLInputElement;
 
 public class CheckoutPage extends BasePage {
 
